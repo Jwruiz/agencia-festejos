e6b13c84ccc879c79bc5f87c70312920e54ef707
Jeheremi R <ruijeremi@gmail.com>
2024-01-23 15:14:45 -0300

    se incorpora nuevamente carrusel

--- src/slider.js
+++ src/slider.js
@@ -1,12 +1,12 @@
 const arrayImg = [
-    './img/DS-1.jpg',
-    './img/DS-2.jpeg',
-    './img/DS-3.jpeg',
-    './img/DS-4.jpeg',
-    './img/DS-5.jpeg',
-    './img/DS-6.jpeg',
-    './img/DS-7.jpeg', 
-    './img/D-cover.jpg',
+    './img/slider-1.JPG',
+    './img/slider-2.jpg',
+    './img/slider-3.jpg',
+    './img/slider-4.jpg',
+    './img/slider-5.jpg',
+    './img/slider-6.jpg',
+    './img/slider-7.jpg', 
+    './img/slider-8.jpg',
 ];
 
 const img = document.querySelector('#slide');
